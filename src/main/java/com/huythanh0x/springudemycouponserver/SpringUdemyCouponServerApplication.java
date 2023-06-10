e61ddec4ffdc22acafea1b9b62ffcb496cc303f1
huythanh0x <huythanh0x@gmail.com>
2023-06-10 22:07:37 +0700

    init

---
+++ src/main/java/com/huythanh0x/springudemycouponserver/SpringUdemyCouponServerApplication.java
@@ -0,0 +1,13 @@
+package com.huythanh0x.springudemycouponserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUdemyCouponServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUdemyCouponServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huythanh0x/springudemycouponserver/SpringUdemyCouponServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huythanh0x.springudemycouponserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUdemyCouponServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
